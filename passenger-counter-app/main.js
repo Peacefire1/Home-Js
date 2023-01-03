cf052a54f2e1db14268d059551ee7c7bacc25e39
Peacefire <ganzorigenkhtsogt@gmail.com>
2023-01-03 18:47:37 +0800

    home

---
+++ passenger-counter-app/main.js
@@ -0,0 +1,20 @@
+
+
+let countEl = document.getElementById("count-el");
+
+
+
+
+let count = 0 ;
+
+function increment(){
+    count += 1
+    
+    countEl.innerText = count
+    // document.getElementById("count-el").innerText += 5;
+
+
+    console.log(count);
+}
+
+
